90ea04b9dfb2e5fcf4a65b9576fb288d5d161fb9
MutheuLinet <muneenilinet@gmail.com>
2022-03-06 19:59:47 +0300

    Add control flows to js file

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,57 @@ function getAkanName() {
     let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
      
-
+    if (MM <= 0 || MM > 12) {
+      alert("Invalid month"); 
+    }    
+    else if (DD <= 0 || DD > 31) {
+      alert("Invalid day");
+    }
+    else if (Math.round(dayBorn) === 0 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender.  Your Akan name is " + maleAkanNames[0] + "!");
+    }
+    else if (Math.round(dayBorn) === 1 || Math.round(dayBorn) === -1 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[1] + "!");
+    }
+    else if (Math.round(dayBorn) === 2 || Math.round(dayBorn) === -2 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[2] + "!");
+    }
+    else if (Math.round(dayBorn) === 3 || Math.round(dayBorn) === -3 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[3] + "!");
+    }
+    else if (Math.round(dayBorn) === 4 || Math.round(dayBorn) === -4 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[4] + "!");
+    }  
+    else if (Math.round(dayBorn) === 5 || Math.round(dayBorn) === -5 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[5] + "!");
+    }
+    else if (Math.round(dayBorn) === 6 || Math.round(dayBorn) === -6 && myGender === "Male") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Male gender. Your Akan name is " + maleAkanNames[6] + "!");
+    }    
+    else if (Math.round (dayBorn) === 0 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[0] + "!")
+    }
+    else if (Math.round(dayBorn) === 1 || Math.round(dayBorn) === -1 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[1] + "!");
+    }
+    else if (Math.round(dayBorn) === 2 || Math.round(dayBorn) === -2 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[2] + "!");
+    }
+    else if (Math.round(dayBorn) === 3 || Math.round(dayBorn) === -3 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[3] + "!");
+    }
+    else if (Math.round(dayBorn) === 4 || Math.round(dayBorn) === -4 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[4] + "!");
+    }
+    else if (Math.round(dayBorn) === 5 || Math.round(dayBorn) === -5 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[5] + "!");
+    }
+    else if (Math.round(dayBorn) === 6 || Math.round(dayBorn) === -6 && myGender === "Female") {
+      document.getElementById("generate-akan").innerHTML = ("You were born on a " + days[dayBorn] + " and are of Female gender. Your Akan name is " + femaleAkanNames[6] + "!");
+    }
+    else {
+      alert("Oops! Something is missing. Please fill out every field");
+    }
 }
 
 function clearAkanOutput(){
